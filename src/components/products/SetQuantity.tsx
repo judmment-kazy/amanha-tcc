7ad3f4a742cfb8ea008d722e4085d774374f8d21
judmment-kazy <4llayjpcg@gmail.com>
2023-10-19 05:59:23 -0300

    traducao

--- src/components/products/SetQuantity.tsx
+++ src/components/products/SetQuantity.tsx
@@ -14,7 +14,7 @@ const btnStyles = 'border-[1.2px] border-slate-300 px-2 rounded';
 const SetQuantity: React.FC<SetQtyProps> = ({cartCounter, cartProduct, handleQtyIncrease, handleQtyDecrease}) => {
     return(
         <div className="flex gap-8 items-center">
-            {cartCounter ? null : <div className="font-semibold">QUANTITY</div>}
+            {cartCounter ? null : <div className="font-semibold">QUANTIDADE</div>}
             <div className="flex gap-4 items-center text-base">
                 <button onClick={handleQtyDecrease} className={btnStyles}>-</button>
                 <div>{cartProduct.quantity}</div>
